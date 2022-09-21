779d645196425abca62dcfa3007ff9d67be26b71
gamingdiamond982 <oj.nologic@gmail.com>
2022-09-21 19:23:31 +0100

    Finished!

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ function calculateTriangleArea(base, height) {
 	return base*height*0.5;
 }
 
-function findHypothenuseSquared(height, base) {
-	return height**2 + base**2;	
+function findHypothenuse(height, base) {
+	return Math.sqrt(height**2 + base**2);
 }
 
 function findBoxVolume(length, width, depth) {
@@ -21,9 +21,43 @@ function findBoxVolume(length, width, depth) {
 }
 
 function findCylinderVolume(height, radius) {
-	return PI*height*radius**2
+	return PI*height*radius**2;
 }
 
 function findSphereVolume(radius) {
-	return (4/3)*PI*r**3
+	return (4/3)*PI*radius**3;
 }
+
+function getInp(id) {
+    return document.getElementById(id).value;
+}
+
+function getCircleArea() {
+    let radius = getInp("circleRadius");
+    alert(calculateCircleArea(radius));
+}
+
+function getRectArea() {
+    alert(calculateRectArea(getInp("rectLength"), getInp("rectHeight")));
+}
+
+function getTriangleArea() {
+    alert(calculateTriangleArea(getInp("triangleHeight"), getInp("triangleBase")));
+}
+
+function getHypo() {
+    alert(findHypothenuse(getInp("hypoOpposite"), getInp("hypoAdjacent")));
+}
+
+function getBoxVolume() {
+    alert(findBoxVolume(getInp("boxLength"), getInp("boxHeight"), getInp("boxDepth")));
+}
+
+function getCylinderVolume() {
+    alert(findCylinderVolume(getInp("cylinderHeight"), getInp("cylinderRadius")));
+}
+
+function getSphereVolume() {
+    alert(findSphereVolume(getInp("sphereRadius")))
+}
+
